b8a7fbc908c2c7d7f5f63e894a0c660cf2b16639
James Spence <jamesaspence20@gmail.com>
2023-02-18 00:08:34 -0600

    feat: make request to /me in ssr to check valid jwt
    This ensures that any page that requires auth makes a request WITH the jwt to ensure the API accepts it too

--- src/util/auth/withAuth.ts
+++ src/util/auth/withAuth.ts
@@ -1,19 +1,31 @@
 import {wrapper} from '@/redux/store';
-import {GetServerSidePropsContext} from 'next';
 import {DecodedJWT, selectToken, selectUser, User} from '@/redux/slices/auth';
 import {Nullable} from '@/types';
+import {cookbookApi} from '@/redux/services/cookbookApi';
 
 export type WithAuthProps = {
   user: User;
   token: DecodedJWT;
 };
 
-export const withAuth = wrapper.getServerSideProps(store => async (ctx: GetServerSidePropsContext) => {
+export const withAuth = wrapper.getServerSideProps(store => async () => {
   const state = store.getState();
 
   const token = selectToken(state);
 
   if (!tokenIsValid(token)) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    };
+  }
+
+  const meResp = await store.dispatch(cookbookApi.endpoints.getMe.initiate(null));
+  await Promise.all(store.dispatch(cookbookApi.util.getRunningQueriesThunk()));
+
+  if (meResp.isError) {
     return {
       redirect: {
         destination: '/login',
