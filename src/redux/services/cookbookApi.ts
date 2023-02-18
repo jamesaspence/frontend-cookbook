c8651b032bee54d70460063ee8fa2d16e8b3f5ec
James Spence <jamesaspence20@gmail.com>
2023-02-17 22:41:02 -0600

    feat: append auth header if present in state

--- src/redux/services/cookbookApi.ts
+++ src/redux/services/cookbookApi.ts
@@ -1,9 +1,19 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import {RootState} from '@/redux/store';
 
 export const cookbookApi = createApi({
   reducerPath: 'cookbook',
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_API_URL
+    baseUrl: process.env.NEXT_PUBLIC_API_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`);
+      }
+
+      return headers;
+    }
   }),
   endpoints: (builder) => ({
     loginUser: builder.mutation({
